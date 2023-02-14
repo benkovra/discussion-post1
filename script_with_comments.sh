e5ce4834a16f53a8c082126acfa299c34ece9a29
benkovra <112646826+benkovra@users.noreply.github.com>
2023-02-14 13:33:07 -0500

    Update script_with_comments.sh

--- script_with_comments.sh
+++ script_with_comments.sh
@@ -11,3 +11,19 @@ open index.hdml #open code#
 code . #opened folder in vscode#
 
 #to put lines into all of the files use echo at the start of each line#
+echo <!DOCTYPE html>
+echo <html lang="en" dir="ltr">
+echo <head>
+echo <meta charset="utf-8">
+echo <meta name="viewport" content="width=device-width, initial-scale=1">
+echo <title>RPS-Benkovich</title>
+echo <link href="css/global.css" rel="stylesheet">
+echo <link href="css/modnorm.css" rel="stylesheet">
+echo <link href="css/style.css" rel="stylesheet">
+echo </head>
+echo <body>
+echo <main>
+echo </main>
+echo <script>
+echo </script>
+echo </html>
